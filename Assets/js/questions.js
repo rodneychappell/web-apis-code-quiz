347d483db97f0584196e628e3bafe288fab83527
Rodney chappell <tookie@Rodneys-Mini.attlocal.net>
2022-01-11 20:03:13 -0500

    commiting rework

--- Assets/js/questions.js
+++ Assets/js/questions.js
@@ -1,87 +1,81 @@
 // list of all questions, choices, and answers
 var questions = [
     {
-      title: "Which one is a looping structure in JavaScript?",
-      choices: ["All the below", "For", "While", "do-while loops"],
-      answer: "All the below"
-    },
-    {
-      title: "What are the two basic groups of data types in JavaScript?",
+      title: "What are two basic groups of data types in JavaScript?",
       choices: [
-        "Primitive and attribute",
-        "Primitive and reference types",
-        "Reference types and attribute",
+        "Attribute & Primitive",
+        "Primitive & reference types",
+        "Attribute & Reference types",
         "None of the above"
       ],
-      answer: "Primitive and reference types"
+      answer: "Attribute & Primitive"
     },
-    {
-      title: "Commonly used data types DO NOT include:",
-      choices: ["strings", "booleans", "alerts", "numbers"],
-      answer: "alerts"
-    },
-    {
-      title: "Boolean operators that can be used in JavaScript include:",
-      choices: [
-        "'And' Operator &&",
-        "'Or' Operator ||",
-        "'Not' Operator !",
-        "All the above"
-      ],
-      answer: "All the above"
-    },
-    {
-      title:
-        "Which one of these is not among the three different types of errors in JavaScript?",
-      choices: [
-        "Animation time errors",
-        "Load time errors",
-        "Run time errors",
-        "Logical Errors"
-      ],
-      answer: "Animation time errors"
-    },
+
     {
       title: "What is the data type of variables in JavaScript?",
       choices: [
-        "Object data types",
         "Function data type",
+        "Object data types",
         "None of the above",
         "All of the above"
       ],
       answer: "Object data types"
     },
+
+     {
+      title:
+        "String values must be enclosed within what when being assigned to variables.",
+      choices: ["commas", "quotes", "parentheses", "curly brackets"],
+      answer: "quotes"
+    },
+
+     {
+      title: "Which one is a looping structure in JavaScript?",
+      choices: ["While", "For", "do-while loops", "All the below"],
+      answer: "All the below"
+    }, 
+
+     {
+      title:
+        "Which one is not among the three different types of errors in JavaScript?",
+      choices: [
+        "Logical Errors",
+        "Run time errors",
+        "Animation time errors",
+        "Load time errors"
+      ],
+      answer: "Animation time errors"
+    },
+   
     {
       title: "The condition in an if / else statement is enclosed within ____.",
-      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      choices: ["quotes", "parentheses", "square brackets", "curly brackets"],
       answer: "parentheses"
     },
+
+    {
+      title: "Commonly used data types DON'T include:",
+      choices: ["numbers", "booleans", "strings", "alerts"],
+      answer: "alerts"
+    },
+    
+    {
+      title:
+        "A very useful tool used during debugging & development for printing content to the debugger is:",
+      choices: ["terminal / bash", "console.log", "for loops", "JavaScript"],
+      answer: "console.log"
+    },
+
     {
       title: "Arrays in JavaScript can be used to store ____.",
       choices: [
-        "numbers and strings",
         "other arrays",
+        "numbers & strings",
         "booleans",
         "all of the above"
       ],
       answer: "all of the above"
     },
-    {
-      title:
-        "String values must be enclosed within ____ when being assigned to variables.",
-      choices: ["commas", "curly brackets", "quotes", "parentheses"],
-      answer: "quotes"
-    },
-    {
-      title:
-        "A very useful tool used during development and debugging for printing content to the debugger is:",
-      choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
-      answer: "console.log"
-    },
-    {
-      title: "What is the type of Pop up boxes available in JavaScript?:",
-      choices: ["Alert", "Confirm", "Prompt", "All the above"],
-      answer: "All the above"
-    }
+    
   ];
   
